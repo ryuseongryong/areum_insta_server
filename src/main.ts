d7d64c11b4b1ba0468e6c0dc2cd5a0786d79249f
ryuseongryong <ryuseongryong@gmail.com>
2021-12-22 16:10:33 +0900

    test : Cors setting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    // maxAge: 1000 * 10 * 1,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
